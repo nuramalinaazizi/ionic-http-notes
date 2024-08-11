86f6b9679729d5ece1071c01c3284849046f02ed
Amalina <azamalina@maxis.com.my>
2024-08-11 12:06:41 +0800

    add pages

---
+++ src/app/readall/readall.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-readall',
+  templateUrl: './readall.page.html',
+  styleUrls: ['./readall.page.scss'],
+})
+export class ReadallPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
